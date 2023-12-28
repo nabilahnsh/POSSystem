cd14660b5a887dc5e56eadf28ac61d352baa3343
nabilahnsh <62099490+nabilahnsh@users.noreply.github.com>
2023-12-28 20:04:10 +0700

    Delete POSSystem2 directory

--- POSSystem2/src/com/kkm/pos2/domain/Tax.java
+++
@@ -1,5 +0,0 @@
-package com.kkm.pos2.domain;
-
-public class Tax {
-	public static double tax = 0.1;
-}
--- POSSystem2/src/com/kkm/pos2/repository/SaleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kkm.pos2.repository;
-
-import com.kkm.pos2.domain.Sale;
-
-public interface SaleRepository {
-	
-	void save(Sale sale);
-	Sale findByNumber(String number);
-}
